e7e5dad693948723ce179a930472a508d9300289
medsep <mehdi.sep@gmail.com>
2020-08-15 22:33:03 +1000

    W5 complete

---
+++ W5/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Mehdi Sepahvand's PHP</title>
+</head>
+<h1>Mehdi Sepahvand First PHP</h1>
+<p>Welcome to my page. Happy browsing!</p>
+<p>The SHA256 hash of "Mehdi Sepahvand" is <strong>
+5a8eda361762b05aed86f78e27a0442a9958491253230e7fbfde5aecad053cc1</strong></p>
+<pre>
+    The AISC art of the first 
+    letter of my name is 
+
+    #     # 
+    ##   ## 
+    # # # # 
+    #  #  # 
+    #     # 
+    #     # 
+    #     # 
+                
+</pre>
+<body>
+<?php
+echo "Thank you for visting my page and reviewing my work! \n";
+echo "The SHA256 hash of Mehdi Sepahvand using PHP is \n"; 
+print hash('sha256', 'Mehdi Sepahvand');
+?>
+</body>
